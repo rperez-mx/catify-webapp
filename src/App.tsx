fa168c5bb17df2a26fad00a58dd8bc493b768f57
Ricardo Pérez <jobs.rperez@gmail.com>
2022-09-12 21:16:45 -0700

    Added basic layout and routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './views/Home'
+import Feed from './components/Feed'
 
 function App() {
   return (
-    <div className="App">
-     Catify Webapp
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path="/feed" element={<Feed />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
